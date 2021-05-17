b78b8d2628bcd93402c0cd17e868d986ff544207
Unilfo <kokopal13@gmail.com>
2021-05-17 23:46:11 +0300

    6

--- client/src/components/customTable.js
+++ client/src/components/customTable.js
@@ -1,16 +1,40 @@
-import {Button} from "antd"
-import { useEffect, useState } from "react";
+import {Button, Modal} from "antd"
+import React, { useEffect, useState } from "react";
 import { Row, Col, Input } from 'antd';
 import "./tb.scss";
+import {
+    Form,
+    Radio,
+    Select,
+    Cascader,
+    DatePicker,
+    InputNumber,
+    TreeSelect,
+    Switch,
+} from 'antd';
 
 const CustomTable = (props) => {
     const [users, setUsers] = useState( () => props.users )
     const [filteredUsers, setFUsers] = useState(() => props.users )
     const [search, setSearch] = useState('')
-
+    const [currentUser, setCurrentUser] = useState(null)
+    const [show, setShow] = useState(false)
     useEffect(() => {
         setFUsers(() => users)
     },[users])
+
+    useEffect(()=>{
+        if(show) {
+            Modal.confirm({
+                title: 'Добовление/редактирование пользователя',
+                content: <FormEdit currentUser/>,
+                okText: 'save',
+                cancelText: 'close',
+                icon: ''
+            })
+        }
+        setShow(false)
+    },[show])
 
     useEffect(()=>{
         if(!users){
@@ -23,9 +47,28 @@ const CustomTable = (props) => {
                 })
             })
         }else{
-            setFUsers( ()=>users ) 
+            setFUsers( ()=>users )
         }
     },[search])
+
+    const FormEdit = (user) => {
+        return(
+            <Form>
+                <Form.Item label="Input">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Password">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Select">
+                    <Select>
+                        <Select.Option value="admin">admin</Select.Option>
+                        <Select.Option value="user">user</Select.Option>
+                    </Select>
+                </Form.Item>
+            </Form>
+        )
+    }
 
     const deleteUser = (id) => {
         let isDel = window.confirm('Вы уверены?')
@@ -33,6 +76,12 @@ const CustomTable = (props) => {
             setUsers(() => users.filter((el) => el.id !== id))
         }
     }
+
+    const RowClick = (user) => {
+        setCurrentUser(user)
+        setShow(true)
+    }
+
 
     return (
         <div>
@@ -52,39 +101,39 @@ const CustomTable = (props) => {
                 <thead>
                        <tr className={'row'}>
                             <td className={'cell'}>
-                                ID 
-                            </td> 
+                                ID
+                            </td>
                             <td className={'cell'}>
-                                Name 
-                            </td>   
+                                Name
+                            </td>
                             <td  className={'cell'}>
-                                Password 
-                            </td>  
+                                Password
+                            </td>
                             <td  className={'cell'}>
-                                Role 
-                            </td>  
+                                Role
+                            </td>
                             <td  className={'cell'}>
                                 Action
-                            </td> 
+                            </td>
                         </tr>
                     </thead>
 
             <tbody>
                 {filteredUsers && filteredUsers.map((el)=>(
-                        <tr className={'row'} key={el.id}>
+                        <tr className={'row'} key={el.id} onClick={() => RowClick(el)}>
                             <td  className={'cell'}>
                                 {el.id}
                             </td>
                             <td  className={'cell'}>
                                 {el.name}
                             </td>
-                            <td  className={'cell'}>                                
+                            <td  className={'cell'}>
                                 {el.password}
                             </td>
                             <td  className={'cell'}>
                                 {el.role}
                             </td>
-                            <td  className={'cell'}>                                
+                            <td  className={'cell'}>
                                 <Button type="primary" size={"large"} onClick={(e)=> deleteUser(el.id)}>
                                     удалить
                                 </Button>
